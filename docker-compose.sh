232657c5ffe503489043d33279bb73d4619cc230
hxzmtk <sadeye@qq.com>
2021-01-21 23:47:29 +0800

    add docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,12 @@
+#!/bin.bash
+
+# see: https://docs.docker.com/compose/install/
+
+
+pushd /tmp >/dev/null
+wget -c https://github.com/docker/compose/releases/download/1.28.0/docker-compose-Linux-x86_64 -O docker-compose && \
+    chmod +x docker-compose && \
+    cp docker-compose /usr/bin/docker-compose && \
+    echo "install success"
+[ $? -eq 0 ] || echo "install faild"
+popd
