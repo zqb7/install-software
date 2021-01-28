966cfc5d0f292b03a30601d876a632386dc307eb
zqhhh <sadeye@qq.com>
2021-01-29 01:04:42 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,14 +2,13 @@
 
 # auto install go
 
-pushd /tmp >/dev/null
 file_url="https://dl.google.com/go/go1.15.6.linux-amd64.tar.gz"
 file_name=`echo $file_url | awk -F '/' '{print $NF}'`
 
-wget -c $file_url && \
+cd /tmp && \ 
+    wget -c $file_url && \
     rm -rf /usr/local/go && \
     tar -C /usr/local -xzf $file_name
-popd >/dev/null
 
 [ $? -eq 0 ] || echo "install faild";exit 1 
 
