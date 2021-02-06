09931922df1ed3a0050d6b2f2c2b053fbd021e78
hxzmtk <sadeye@qq.com>
2021-02-07 00:13:08 +0800

    add protoc.sh

---
+++ protoc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+VERSION="3.14.0"
+
+if [ -n $1 ];then
+    VERSION=$1
+fi
+
+FILENAME=protobuf-all-${VERSION}.tar.gz
+FILEURL=https://github.com/protocolbuffers/protobuf/releases/download/v${VERSION}/${FILENAME}
+cd /tmp && wget -c ${FILEURL} && \
+    tar zxvf  ${FILENAME} && \
+    cd protobuf-${VERSION} && \
+    ./configure && \
+    make install && \
+    ldconfig
+
+if [ $? -ne 0 ];then 
+    echo "install faild"
+    exit 1 
+fi
+echo "install protoc success!"
+
+# if run protoc is error 
+# ex: protoc: error while loading shared libraries: libprotoc.so.18: cannot open shared object file: No such file or directory
+# run `ldconfig`
