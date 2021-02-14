3d4c3eedeb508731c796a8e04747bc84aaf72355
zqhhh <sadeye@qq.com>
2021-02-14 19:22:41 +0800

    update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,14 +1,11 @@
 #!/bin.bash
 
 # see: https://docs.docker.com/compose/install/
+VERSION="1.28.0"
+if [ -n "$1" ];then VERSION=$1; fi
 
-
-pushd /tmp >/dev/null
-wget -c https://github.com/docker/compose/releases/download/1.28.0/docker-compose-Linux-x86_64 -O docker-compose && \
+cd /tmp && wget -c https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-Linux-x86_64 -O docker-compose && \
     chmod +x docker-compose && \
     cp docker-compose /usr/bin/docker-compose && \
     echo "install success"
-if [ $? -ne 0 ];then 
-    echo "install faild"
-fi
-popd
+if [ $? -ne 0 ];then echo "install faild"; fi
