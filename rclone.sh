04984704f9f21f4332e2ec5c0f6bab1ab9c680f5
zqhhh <sadeye@qq.com>
2021-02-26 00:44:33 +0800

    add sh

---
+++ rclone.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION="1.54.0"
+
+if [ -n "$1" ];then
+    VERSION=$1
+fi
+
+
+FILEURL=https://github.com/rclone/rclone/releases/download/v${VERSION}/rclone-v${VERSION}-linux-amd64.zip
+FILENAME=rclone-v${VERSION}-linux-amd64.zip
+
+
+cd /tmp && wget -c $FILEURL && \
+    unzip -nq  ${FILENAME} && \
+    cd ${FILENAME/.zip} && \
+    cp rclone /usr/local/bin/rclone
+
+if [ $? -ne 0 ];then 
+    echo "install faild"
+    exit 1
+else
+    echo "install rclone  ${VERSION} success"
+fi
---
+++ syncthing.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION="1.13.1"
+
+if [ -n "$1" ];then
+    VERSION=$1
+fi
+
+
+FILEURL=https://github.com/syncthing/syncthing/releases/download/v${VERSION}/syncthing-linux-amd64-v${VERSION}.tar.gz
+FILENAME=syncthing-linux-amd64-v${VERSION}.tar.gz
+
+
+cd /tmp && wget -c $FILEURL && \
+    tar zxvf  ${FILENAME} && \
+    cd ${FILENAME/.tar.gz} && \
+    cp syncthing /usr/local/bin/syncthing
+
+if [ $? -ne 0 ];then 
+    echo "install faild"
+    exit 1
+else
+    echo "install syncthing  ${VERSION} success"
+fi
