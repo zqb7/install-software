dd5559c018ee23d768c760bb7c5bc5eb279847e9
zqhhh <sadeye@qq.com>
2021-08-25 02:00:09 +0000

    update firefox.sh:91.0.2

--- firefox.sh
+++ firefox.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 pushd /tmp >/dev/null
-fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/91.0.1/linux-x86_64/zh-CN/firefox-91.0.1.tar.bz2"
+fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/91.0.2/linux-x86_64/zh-CN/firefox-91.0.2.tar.bz2"
 fileName="Firefox-latest-x86_64.tar.bz2"
 
 wget -c $fileUrl -O $fileName || exit 1
