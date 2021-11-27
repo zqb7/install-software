f4414f140c56fc4d5f8406309b6730afedcb89ae
zqhhh <sadeye@qq.com>
2021-11-27 02:00:11 +0000

    update tigervnc.sh:latest

--- tigervnc.sh
+++ tigervnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20211124git4a761222.x86_64.tar.gz"
-fileName="tigervnc-1.12.80-20211124git4a761222.x86_64.tar.gz"
+fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20211126git0c5a2b2e.x86_64.tar.gz"
+fileName="tigervnc-1.12.80-20211126git0c5a2b2e.x86_64.tar.gz"
 
 export HOME=${HOME}
 
