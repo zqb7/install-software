10373955076bb44b036fb9d6eff9257c4246748f
zqhhh <sadeye@qq.com>
2021-12-09 03:01:06 +0000

    fix

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -4,12 +4,15 @@ _main() {
     which sudo >/dev/null && SUDO="sudo"
     which zsh > /dev/null || ${SUDO} apt-get install -y zsh
 
-    [ -d ~/.oh-my-zsh ] \
-    || git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh \
-    && [ -f ~/.zshrc ] &&  cp ~/.zshrc ~/.zshrc.orig \
-    && cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc \
-    && ${SUDO} chsh -s "$(which zsh)" \
-    && echo "install oh-my-zsh success"
+    if [ -d ~/.oh-my-zsh ];then
+        echo "has been installed"
+    else
+        git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh \
+        && [ -f ~/.zshrc ] &&  cp ~/.zshrc ~/.zshrc.orig \
+        && cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc \
+        && ${SUDO} chsh -s "$(which zsh)" \
+        && echo "install oh-my-zsh success"
+    fi
 }
 
 _main
