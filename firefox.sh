a6d434a1b815eaa6d62e15a3c4ce6e0f935216f6
zqhhh <sadeye@qq.com>
2022-01-21 02:00:11 +0000

    update firefox.sh:96.0.2

--- firefox.sh
+++ firefox.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/96.0.1/linux-x86_64/zh-CN/firefox-96.0.1.tar.bz2"
+fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/96.0.2/linux-x86_64/zh-CN/firefox-96.0.2.tar.bz2"
 fileName="Firefox-latest-x86_64.tar.bz2"
 
 _main() {
