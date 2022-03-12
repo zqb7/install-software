786a946146e0de958c257ed8f6fca545345f307e
zqhhh <sadeye@qq.com>
2022-03-12 02:00:10 +0000

    update hugo.sh:v0.94.1,tigervnc.sh:latest

--- tigervnc.sh
+++ tigervnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20220310git67217afa.x86_64.tar.gz"
-fileName="tigervnc-1.12.80-20220310git67217afa.x86_64.tar.gz"
+fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20220311gitadc4570b.x86_64.tar.gz"
+fileName="tigervnc-1.12.80-20220311gitadc4570b.x86_64.tar.gz"
 
 export HOME=${HOME}
 
