3b406a29585a8827ae95eea7d5f5450dc2827c55
zqhhh <sadeye@qq.com>
2022-04-15 02:00:11 +0000

    update code-server.sh:v4.3.0,hugo.sh:v0.97.0

--- code-server.sh
+++ code-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v4.2.0"
+VERSION="v4.3.0"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
--- hugo.sh
+++ hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v0.96.0"
+VERSION="v0.97.0"
 
 if [ -n "$1" ];then
     VERSION=$1
