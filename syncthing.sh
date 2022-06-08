a4519a79b1cb888f7bb427986cb0eda23e705eb2
zqhhh <sadeye@qq.com>
2022-06-08 02:00:10 +0000

    update syncthing.sh:v1.20.2,tigervnc.sh:latest

--- syncthing.sh
+++ syncthing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v1.20.1"
+VERSION="v1.20.2"
 
 if [ -n "$1" ];then
     VERSION=$1
--- tigervnc.sh
+++ tigervnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20220512git15fc3e2a.x86_64.tar.gz"
-fileName="tigervnc-1.12.80-20220512git15fc3e2a.x86_64.tar.gz"
+fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20220607git20e42bfe.x86_64.tar.gz"
+fileName="tigervnc-1.12.80-20220607git20e42bfe.x86_64.tar.gz"
 
 export HOME=${HOME}
 
