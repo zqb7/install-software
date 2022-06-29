a161d78a37bc245b7c36e697a1899f5be43052eb
zqhhh <sadeye@qq.com>
2022-06-29 02:00:10 +0000

    update firefox.sh:102.0,tigervnc.sh:latest

--- firefox.sh
+++ firefox.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/101.0.1/linux-x86_64/zh-CN/firefox-101.0.1.tar.bz2"
+fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/102.0/linux-x86_64/zh-CN/firefox-102.0.tar.bz2"
 fileName="Firefox-latest-x86_64.tar.bz2"
 
 _main() {
