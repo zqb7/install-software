8f78e4762612e3c319ac56ed64dcc670e683949d
zqhhh <sadeye@qq.com>
2022-09-03 02:00:14 +0000

    update drawio.sh:v20.2.8,tigervnc.sh:latest

--- drawio.sh
+++ drawio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v20.2.3"
+VERSION="v20.2.8"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
--- tigervnc.sh
+++ tigervnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20220901git72ef9211.x86_64.tar.gz"
-fileName="tigervnc-1.12.80-20220901git72ef9211.x86_64.tar.gz"
+fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20220902git645c1f2a.x86_64.tar.gz"
+fileName="tigervnc-1.12.80-20220902git645c1f2a.x86_64.tar.gz"
 
 export HOME=${HOME}
 
