645d598858046044ec13e616fc78ca4bb1c7790d
zqhhh <sadeye@qq.com>
2022-09-21 02:00:12 +0000

    update docker-compose.sh:v2.11.1,firefox.sh:105.0

--- firefox.sh
+++ firefox.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/104.0.2/linux-x86_64/zh-CN/firefox-104.0.2.tar.bz2"
+fileUrl="https://download-installer.cdn.mozilla.net/pub/firefox/releases/105.0/linux-x86_64/zh-CN/firefox-105.0.tar.bz2"
 fileName="Firefox-latest-x86_64.tar.bz2"
 
 _main() {
