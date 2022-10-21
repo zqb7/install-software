10fac965c42e29da48c46679d738cbf1d98ea82f
binlihpu <33745963+binlihpu@users.noreply.github.com>
2022-10-21 13:05:49 +0800

    Update go.sh
    安装go时判断使用的bash还是zsh并将变量写入对应文件

--- go.sh
+++ go.sh
@@ -7,6 +7,8 @@ FILENAME=go${VERSION}.linux-amd64.tar.gz
 
 SUDO=''
 
+GOVAR="\n# Go environment variable\nexport PATH=\$PATH:/usr/local/go/bin\nexport GOPROXY=https://goproxy.cn,direct\nexport GO111MODULE=on"
+
 _init() {
     which sudo && SUDO="sudo"
 }
@@ -16,10 +18,17 @@ _main() {
     && curl -O -C - $FILEURL \
     && ${SUDO} rm -rf /usr/local/go \
     && ${SUDO} tar -C /usr/local -xzf $FILENAME \
-    && echo "export PATH=\$PATH:/usr/local/go/bin\nexport GOPROXY=https://goproxy.cn,direct\nexport GO111MODULE=on" | ${SUDO} tee  /etc/profile.d/go.sh \
-    && echo "install go v${VERSION} success
-You may need run \`source /etc/profile\`
-    "
+    && echo "install go v${VERSION} success"
+
+    if [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
+        echo ${GOVAR} | tee -a ~/.bashrc
+        echo "You may need run \`source ~/.bashrc\`"
+    elif [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+        echo ${GOVAR} | tee -a ~/.zshrc 
+        echo "You may need run \`source ~/.zshrc\`"
+    else
+        echo -e "You may add manually \n${GOVAR}"
+    fi
 }
 
 _init
