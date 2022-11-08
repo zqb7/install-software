696f088a3b535da7895165736882507a2c54a312
zqhhh <sadeye@qq.com>
2022-11-08 02:00:12 +0000

    update code-server.sh:v4.8.3,tigervnc.sh:latest

--- code-server.sh
+++ code-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v4.8.2"
+VERSION="v4.8.3"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
--- tigervnc.sh
+++ tigervnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20221101git4ddd7f47.x86_64.tar.gz"
-fileName="tigervnc-1.12.80-20221101git4ddd7f47.x86_64.tar.gz"
+fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.12.80-20221107gitdd8d208e.x86_64.tar.gz"
+fileName="tigervnc-1.12.80-20221107gitdd8d208e.x86_64.tar.gz"
 
 export HOME=${HOME}
 
