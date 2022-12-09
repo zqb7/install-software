e8ee6e98da8c30b61bc5a55eb08a896998c05f31
zqhhh <sadeye@qq.com>
2022-12-09 02:00:12 +0000

    update drawio.sh:v20.6.1,protoc.sh:v21.11

--- drawio.sh
+++ drawio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v20.3.0"
+VERSION="v20.6.1"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
--- protoc.sh
+++ protoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v21.10"
+VERSION="v21.11"
 
 if [ -n "$1" ];then
     VERSION=$1
