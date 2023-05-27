97f6d6c460e06723b184c9880ebe048929c59ff7
zqhhh <sadeye@qq.com>
2023-05-27 01:30:24 +0000

    perf:版本号3.0.0+的，直接使用二进制

--- protoc.sh
+++ protoc.sh
@@ -6,20 +6,22 @@ if [ -n "$1" ];then
     VERSION=$1
 fi
 
-FILENAME=protobuf-all-${VERSION#v}.tar.gz
-FILEURL=https://github.com/protocolbuffers/protobuf/releases/download/v${VERSION#v}/${FILENAME#v}
+which sudo >/dev/null && SUDO="sudo"
 
-_main() {
-    which sudo >/dev/null && SUDO="sudo"
+_install_v_3_0_0() {
+    FILENAME=protoc-${VERSION#v}-linux-x86_64.zip
+    FILEURL=https://github.com/protocolbuffers/protobuf/releases/download/v${VERSION#v}/${FILENAME#v}
+    cd /tmp \
+    && wget -c ${FILEURL} \
+    && chmod 666 ${FILENAME} \
+    && unzip -o ${FILENAME} -d protoc-${VERSION} \
+    && ${SUDO} cp protoc-${VERSION}/bin/protoc /usr/local/bin/protoc \
+    && echo "install protoc ${VERSION} success"
+}
 
-    http_code=`curl -s -o /dev/null -I -w "%{http_code}" ${FILEURL}`
-    if [ $http_code -eq 404 ];then
-        FILENAME=protobuf-${VERSION#v}.tar.gz
-        FILEURL=https://github.com/protocolbuffers/protobuf/releases/download/v${VERSION#v}/${FILENAME#v}
-    elif [ $http_code -ne 200 ] && [ $http_code -ne 301 ] && [ $http_code -ne 302 ];then
-        echo "http ${http_code}"
-        exit 1
-    fi
+_install_old() {
+    FILENAME=protobuf-${VERSION#v}.tar.gz
+    FILEURL=https://github.com/protocolbuffers/protobuf/releases/download/v${VERSION#v}/${FILENAME#v}
     cd /tmp \
     && wget -c ${FILEURL} \
     && chmod 666 ${FILENAME} \
@@ -34,4 +36,8 @@ _main() {
 # ex: protoc: error while loading shared libraries: libprotoc.so.18: cannot open shared object file: No such file or directory
 # run `ldconfig`
 
-_main
+if [[ "$(echo -e "${VERSION#v}\n3.0.0" | sort -V | head -n2)" == "${VERSION#v}" ]]; then
+    _install_v_3_0_0
+else
+    _install_old
+fi
