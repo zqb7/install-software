da098ee939bde9f28617eb6fb4e87f1dd3db49ea
zqhhh <sadeye@qq.com>
2023-06-30 00:44:36 +0800

    Update drawio.sh

--- drawio.sh
+++ drawio.sh
@@ -17,7 +17,7 @@ _main() {
     && ${SUDO} chmod +x /opt/drawio/drawio.AppImage
 
     [ $? -ne 0 ] && return
-    cat << EOF > /opt/drawio/drawio.svg
+    ${SUDO} cat << EOF > /opt/drawio/drawio.svg
 <?xml version="1.0" encoding="utf-8"?>
 <!-- Generator: Adobe Illustrator 21.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
