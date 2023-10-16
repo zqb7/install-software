46b05efffa0cc124be57732577b281270918be2d
zqhhh <sadeye@qq.com>
2023-10-17 07:11:42 +0800

    update qq.sh:latest

--- qq.sh
+++ qq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILEURL="https://dldir1.qq.com/qqfile/qq/QQNT/5b1d2011/linuxqq_3.2.1-17260_x86_64.AppImage"
+FILEURL="https://dldir1.qq.com/qqfile/qq/QQNT/423936b9/linuxqq_3.2.1-17412_x86_64.AppImage"
 FILENAME=qq.AppImage
 _main() {
     which sudo >/dev/null && SUDO="sudo"
