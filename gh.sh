8f959ddd69c7c8c7f2d2affc04085f77d6cd911a
qilook <sadeye@qq.com>
2023-10-18 07:16:40 +0800

    update gh.sh:v2.37.0,node.sh:v21.0.0

--- gh.sh
+++ gh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v2.36.0"
+VERSION="v2.37.0"
 
 if [ -n "$1" ];then
     VERSION=$1
--- node.sh
+++ node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # auto install nodejs
-VERSION="v20.8.1"
+VERSION="v21.0.0"
 
 if [ -n "$1" ];then
     VERSION=$1
