817a418837bff26be37683f4ca2523fc52128278
qilook <sadeye@qq.com>
2023-11-17 07:06:15 +0800

    update docker-compose.sh:v2.23.1,drawio.sh:v22.1.2

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # see: https://docs.docker.com/compose/install/
-VERSION="v2.23.0"
+VERSION="v2.23.1"
 if [ -n "$1" ];then VERSION=$1; fi
 
 _main() {
--- drawio.sh
+++ drawio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v22.0.3"
+VERSION="v22.1.2"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
