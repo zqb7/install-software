fcb83f769a427fefc5110ab17b477c4306b5d6fb
qilook <sadeye@qq.com>
2024-01-26 07:04:29 +0800

    update tigervnc.sh:latest,qq.sh:latest

--- tigervnc.sh
+++ tigervnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.13.80-20240124gitfe4b0145.x86_64.tar.gz"
-fileName="tigervnc-1.13.80-20240124gitfe4b0145.x86_64.tar.gz"
+fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.13.80-20240125git238028d9.x86_64.tar.gz"
+fileName="tigervnc-1.13.80-20240125git238028d9.x86_64.tar.gz"
 
 export HOME=${HOME}
 
