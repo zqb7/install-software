43e9bf31b1fcd6b46dcc5961a1657b39aad0e080
zqb7 <sadeye@qq.com>
2024-03-06 07:10:29 +0800

    update firefox.sh:123.0.1,go.sh:1.22.1,syncthing.sh:v1.27.4

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="1.22.0"
+VERSION="1.22.1"
 
 if [ -n "$1" ];then VERSION=$1;fi
 
--- syncthing.sh
+++ syncthing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v1.27.3"
+VERSION="v1.27.4"
 
 if [ -n "$1" ];then
     VERSION=$1
