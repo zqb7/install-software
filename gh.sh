8a0142e910847d41f206a3fff0845f8897228a09
zqb7 <sadeye@qq.com>
2024-03-21 07:29:50 +0800

    update gh.sh:v2.46.0,hugo.sh:v0.124.1

--- gh.sh
+++ gh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v2.45.0"
+VERSION="v2.46.0"
 
 if [ -n "$1" ];then
     VERSION=$1
--- hugo.sh
+++ hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v0.124.0"
+VERSION="v0.124.1"
 
 if [ -n "$1" ];then
     VERSION=$1
