caa3fd6d6219f5b4b3fdfb59dd7a50a0cae3cfb5
zqb7 <sadeye@qq.com>
2024-04-19 07:04:52 +0800

    update hugo.sh:v0.125.1,protoc.sh:v27.0-rc1

--- hugo.sh
+++ hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v0.125.0"
+VERSION="v0.125.1"
 
 if [ -n "$1" ];then
     VERSION=$1
--- protoc.sh
+++ protoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v26.1"
+VERSION="v27.0-rc1"
 
 if [ -n "$1" ];then
     VERSION=$1
