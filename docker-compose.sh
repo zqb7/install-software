63fb16cc404bb83cc9027c1107162e429078e3b1
zqb7 <sadeye@qq.com>
2024-04-25 07:02:29 +0800

    update docker-compose.sh:v2.27.0,node.sh:v22.0.0

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # see: https://docs.docker.com/compose/install/
-VERSION="v2.26.1"
+VERSION="v2.27.0"
 if [ -n "$1" ];then VERSION=$1; fi
 
 _main() {
--- node.sh
+++ node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # auto install nodejs
-VERSION="v21.7.3"
+VERSION="v22.0.0"
 
 if [ -n "$1" ];then
     VERSION=$1
