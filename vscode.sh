92dd96325265d2eb71e7e825df08b1232b477eda
zqb7 <sadeye@qq.com>
2024-05-15 19:26:53 +0800

    修复vscode登录帐号时，无法跳转

--- vscode.sh
+++ vscode.sh
@@ -18,8 +18,8 @@ _main() {
         exit 1
     fi
     ${SUDO} cp ${SHELL_FOLDER}/icon/vscode.png /opt/VSCode-linux-x64/vscode.png \
-    ${SUDO} cp ${SHELL_FOLDER}/desktop/vscode.desktop /opt/VSCode-linux-x64/vscode.desktop \
-    && ${SUDO} ln -fs  /opt/VSCode-linux-x64/vscode.desktop /usr/share/applications/code.desktop \
+    && ${SUDO} cp ${SHELL_FOLDER}/desktop/code.desktop /usr/share/applications/code.desktop \
+    && ${SUDO} cp ${SHELL_FOLDER}/desktop/code-url-handler.desktop /usr/share/applications/code-url-handler.desktop \
     && ${SUDO} ln -fs /opt/VSCode-linux-x64/code /usr/local/bin/code \
     && ${SUDO} ln -fs /opt/VSCode-linux-x64/code-tunnel /usr/local/bin/code-tunnel \
     && echo "install vscode latest success"
