d86e42fd30a02a5edb106862964b1bba0c4cc0b3
zqb7 <sadeye@qq.com>
2024-05-23 07:01:50 +0800

    update drawio.sh:v24.4.6,joplin.sh:v2.14.22

--- drawio.sh
+++ drawio.sh
@@ -2,7 +2,7 @@
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 
-VERSION="v24.4.0"
+VERSION="v24.4.6"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
--- joplin.sh
+++ joplin.sh
@@ -2,7 +2,7 @@
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 
-VERSION="v2.14.20"
+VERSION="v2.14.22"
 
 if [ -n "$1" ];then VERSION=$1; fi
 
