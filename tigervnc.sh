08a7e8db38eee6adf717c290bddfdc62e848697c
zqb7 <sadeye@qq.com>
2024-06-21 06:59:29 +0800

    update tigervnc.sh:latest

--- tigervnc.sh
+++ tigervnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.13.80-20240619git4812f13a.x86_64.tar.gz"
-fileName="tigervnc-1.13.80-20240619git4812f13a.x86_64.tar.gz"
+fileUrl="http://tigervnc.bphinz.com/nightly/xc/x86_64/tigervnc-1.14.80-20240620git48408d18.x86_64.tar.gz"
+fileName="tigervnc-1.14.80-20240620git48408d18.x86_64.tar.gz"
 
 export HOME=${HOME}
 
