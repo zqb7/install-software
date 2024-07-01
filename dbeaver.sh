4ea99c27315671edd4cfd35ba4d687c20d9644a1
zqb7 <sadeye@qq.com>
2024-07-01 17:40:29 +0800

    下载的文件存放致当前tmp目录

--- dbeaver.sh
+++ dbeaver.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
+TMP_FOLDER=${SHELL_FOLDER}/tmp
 
 _main() {
     which sudo >/dev/null && SUDO="sudo"
 
-    cd /tmp \
+    cd ${TMP_FOLDER} \
     && wget --max-redirect=4 -c https://dbeaver.io/files/dbeaver-ce-latest-linux.gtk.x86_64.tar.gz \
     && chmod 666 dbeaver-ce-latest-linux.gtk.x86_64.tar.gz \
     && ${SUDO} tar -zxvf dbeaver-ce-latest-linux.gtk.x86_64.tar.gz -C /opt \
