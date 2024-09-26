8b268e438ea7c2e7670f9a54e4dcbc9d88d14ffd
runner <runner@fv-az1980-251>
2024-09-26 01:22:01 +0000

    update qq.sh:latest

--- qq.sh
+++ qq.sh
@@ -2,7 +2,7 @@
 
 SHELL_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]});pwd)
 
-FILEURL="https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.8_240520_x86_64_01.AppImage"
+FILEURL="https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_amd64_01.deb","rpm":"https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_x86_64_01.rpm","appimage":"https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_x86_64_01.AppImage"},"armDownloadUrl":{"deb":"https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_arm64_01.deb","rpm":"https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_aarch64_01.rpm","appimage":"https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_arm64_01.AppImage"},"loongarchDownloadUrl":{"deb":"https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_loongarch64_01.deb"},"mipsDownloadUrl":{"deb":"https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_mips64el_01.deb"}};"
 FILENAME=qq.AppImage
 _main() {
     which sudo >/dev/null && SUDO="sudo"
