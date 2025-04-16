79c2d8bf2f201679a159c04751bcebc7d6228efe
runner <runner@fv-az1695-55>
2025-04-16 01:40:50 +0000

    update firefox.sh:137.0.2,gh.sh:v2.70.0,hugo.sh:v0.146.5

--- gh.sh
+++ gh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v2.69.0"
+VERSION="v2.70.0"
 
 if [ -n "$1" ];then
     VERSION=$1
--- hugo.sh
+++ hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="v0.146.4"
+VERSION="v0.146.5"
 
 if [ -n "$1" ];then
     VERSION=$1
